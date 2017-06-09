d3aa66ac0e9ca563ba8939e5e4285e4702dd80fd
Karol Wyskocki <karol.wyskocki@vp.pl>
2017-06-09 14:33:02 +0200

    dodano parsowanie jsona z serwera

--- Src/HTTPrequest.c
+++ Src/HTTPrequest.c
@@ -9,22 +9,28 @@
 #include "wifiModule.h"
 #include "string.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "uart.h"
+#include "jsmn.h"
 
-char httpBuffer[512];
+char httpBuffer[256];
+
+static int8_t jsoneq(const char *json, jsmntok_t *tok, const char *s);
 
 RequestResult cardRequest(UART_HandleTypeDef * WiFi, UART_HandleTypeDef * uart, uint8_t* cardID, Terminal_mode mode, char* hostAdress, char* port){
 	RequestResult result;
 	result.HTTP_status = CHECK_ERROR;
 	result.wifiStatus = WiFi_OK;
 	result.HTTP_status = 0;
+	jsmn_parser jsonParser;
+	jsmntok_t jsonTokens [6];
 
 	char httpBody[48];
-	uint bodySize = 0;
+	uint size = 0;
 
+	//Przygotowanie zapytania
 
-
-	bodySize = sprintf(httpBody, "{\"Mode\":\"%s\",\"CardID\":\"%02X%02X%02X%02X%02X\"}",
+	size = sprintf(httpBody, "{\"Mode\":\"%s\",\"CardID\":\"%02X%02X%02X%02X%02X\"}",
 			mode == enterMode ? "enter" : "exit",
 					cardID[0],
 					cardID[1],
@@ -33,29 +39,80 @@ RequestResult cardRequest(UART_HandleTypeDef * WiFi, UART_HandleTypeDef * uart, 
 					cardID[4]);
 
 	sprintf(httpBuffer, "POST /RFIDterminal HTTP/1.1\r\nHost: %s:%s\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n",
-			hostAdress, port, bodySize);
+			hostAdress, port, size);
 
 	strcat(httpBuffer, httpBody);
 
 	//DEBUG!
 	HAL_UART_Transmit(uart, (uint8_t *)httpBuffer, strlen(httpBuffer), 1000);
 
+
+	//wys³anie zapytania
 	result.wifiStatus = WiFi_openConnection(WiFi, hostAdress, port);
 
 	if(result.wifiStatus == WiFi_OK){
+
 		WiFi_sendData(WiFi, httpBuffer, strlen(httpBuffer));
-		bodySize = WiFi_readData(WiFi, httpBuffer, sizeof httpBuffer / sizeof *httpBuffer, 5000);
-		bodySize += WiFi_readData(WiFi, httpBuffer+bodySize, sizeof httpBuffer / sizeof *httpBuffer-bodySize, 5000);
 
-		//WiFi_closeConnection(WiFi);
-		//sprintf(httpBody, "%d", bodySize);
-		//uartWriteLine(uart, (uint8_t *)httpBody);
-		httpBuffer[bodySize] = '\0';
-		//DEBUG!
-		HAL_UART_Transmit(uart, (uint8_t *)httpBuffer, strlen(httpBuffer), 1000);
+		//odbiór odpowiedzi
+		size = WiFi_readData(WiFi, httpBuffer, sizeof httpBuffer / sizeof *httpBuffer, 5000);
+		size += WiFi_readData(WiFi, httpBuffer+size, (sizeof httpBuffer-size) / sizeof *httpBuffer, 5000);
 
-		result.HTTP_status = 200;
-		result.result = ACCESS_DENIED;
+		httpBuffer[size] = '\0';
+
+		//interpretacja nag³ówka
+		result.HTTP_status = atoi(httpBuffer+9);
+
+		if(result.HTTP_status == 200){
+			//interpretacja odebranych danych
+
+			char* responseBody = strstr(httpBuffer, "\r\n\r\n")+4;	//\r\n\r\n -> 4 znaki
+
+			//DEBUG!
+			HAL_UART_Transmit(uart, (uint8_t *)httpBuffer, strlen(httpBuffer), 1000);
+			uartWriteLine(uart, "Response body:");
+			HAL_UART_Transmit(uart, (uint8_t *)responseBody, strlen(responseBody), 1000);
+
+
+
+			jsmn_init(&jsonParser);
+			int8_t numOfTokens = jsmn_parse(&jsonParser, responseBody, strlen(responseBody), jsonTokens, sizeof(jsonTokens)/sizeof(jsonTokens[0]));
+
+			if(numOfTokens < 0){
+				uartWriteLine(uart, "Parse Error");
+				result.result = CHECK_ERROR;
+				return result;
+			}
+
+			if (numOfTokens < 1 || jsonTokens[0].type != JSMN_OBJECT) {
+				uartWriteLine(uart, "Object expected");
+				result.result = CHECK_ERROR;
+				return result;
+			}
+
+			for(uint8_t i = 1; i < numOfTokens; i++){
+				if(jsoneq(responseBody, &jsonTokens[i], "Access") == 0){
+					if(jsoneq(responseBody, &jsonTokens[i+1], "granted") == 0){
+						result.result = ACCESS_GRANTED;
+						uartWriteLine(uart, "ACCESS_GRANTED");
+					}else if(jsoneq(responseBody, &jsonTokens[i+1], "denied") == 0){
+						result.result = ACCESS_DENIED;
+						uartWriteLine(uart, "ACCESS_DENIED");
+					}else{
+						result.result = CHECK_ERROR;
+						uartWriteLine(uart, "CHECK_ERROR");
+					}
+				}
+			}
+		}
 	}
 	return result;
+}
+
+static int8_t jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
+			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+		return 0;
+	}
+	return -1;
 }
